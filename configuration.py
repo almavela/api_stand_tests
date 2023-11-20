f69e7f30f3a7e311ea7faac3acc7dfd9ab146f1b
Alma Vela <qa.almavela@gmail.com>
2023-11-19 22:28:44 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE = "https://dd75a4bb-8df9-4216-9592-6e5e88634572.serverhub.tripleten-services.com"
+DOC_PATH = "C:\Users\almav\PycharmProjects/"
---
+++ sender_stand_request.py
@@ -0,0 +1,5 @@
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+response = get_users_table()
+print(response.status_code)
